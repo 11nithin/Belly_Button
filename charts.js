df4ca3d4ae153658f2752d14ae8f2ea7d9aaa4ff
11nithin <nithinjacobjames@hotmail.com>
2021-11-21 17:13:08 -0500

    bubble data

--- charts.js
+++ charts.js
@@ -99,5 +99,31 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout)
+
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: 'Earth'
+        
+      }
+
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      xaxis: { title: "OTU"},
+      hovermode: "closest",
+      height: 600,
+      width: 1200
+    };
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout)
   });
 }
